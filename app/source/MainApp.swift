95558bafccac320bb5186ffe21d0b33b67a8848c
Matt Robinson <mattrob@hey.com>
2024-07-30 16:45:08 -0600

    Test launch images
    Signed-off-by: Matt Robinson <mattrob@hey.com>

--- app/source/MainApp.swift
+++ app/source/MainApp.swift
@@ -1,6 +1,16 @@
 import SwiftUI
 
 @main
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Make it go slow to see launch image....
+        for i in 0...100000 {
+          print(i)
+        }
+        return true
+    }
+}
+
 struct MainApp: App {
     var body: some Scene {
         WindowGroup {

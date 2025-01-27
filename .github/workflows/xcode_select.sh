3fc4cfe203bf2c5794434d270bb0d6aec5ea5f6b
Matt Robinson <mattrob@hey.com>
2025-01-27 10:11:10 -0700

    Update dependencies and Xcode
    Signed-off-by: Matt Robinson <mattrob@hey.com>

--- .github/workflows/xcode_select.sh
+++ .github/workflows/xcode_select.sh
@@ -5,7 +5,7 @@ echo "Selecting Xcode for environment"
 printenv
 
 sudo xcode-select -p
-sudo xcode-select -s /Applications/Xcode_15.2.app
+sudo xcode-select -s /Applications/Xcode_15.4.app
 
 echo "Generating bazelrc"
 
